bbb1d12fccca5b3f80fa1ca5d38d8c32cdd646b3
Aaron Long <along@triu.com>
2012-06-12 15:10:53 -0400

    Implement guard type logic to run tests as they change

--- scripts/Spork.groovy
+++ scripts/Spork.groovy
@@ -1,6 +1,5 @@
-import org.codehaus.groovy.grails.test.junit4.JUnit4GrailsTestType
 import groovyx.net.http.HTTPBuilder
-import groovyx.net.http.Method
+import org.codehaus.groovy.grails.test.junit4.JUnit4GrailsTestType
 
 includeTargets << grailsScript("_GrailsClean")
 includeTargets << grailsScript("_GrailsTest")
@@ -12,18 +11,8 @@ serverHost = System.getProperty("server.host") ?: "localhost"
 serverPort = System.getProperty("server.port") ?: "8080"
 
 target(main: "Run integration test(s) inside a running server") {
-    def tests
-
-    // Re-run failed tests
-    reRunTests = argsMap["rerun"]
-    if( reRunTests ) {
-        tests = getFailedTests()
-    }
-    else {
-        tests = argsMap.params
-    }
-
     // If we are given tests to run, run them and exit. Otherwise, just loop and run in guard mode
+    def tests = argsMap.params
     if( tests ) {
         compile()
         runTests(tests)
@@ -34,18 +23,39 @@ target(main: "Run integration test(s) inside a running server") {
 }
 
 def watchForTestChanges() {
-    grailsConsole.updateStatus "Checking for test changes every ${recompileFrequence} seconds..."
+    grailsConsole.addStatus "Checking for test changes every ${recompileFrequence} seconds..."
+    long lastScanned = 0
 
     //noinspection GroovyInfiniteLoopStatement
     while(true) {
+        def modifiedFiles = []
+        if( lastScanned ) {
+            modifiedFiles = findUpdatedTests(lastScanned)
+        }
+
+        // If anything was modified, compile and run
+        if( modifiedFiles ) {
+            modifiedFiles.each { grailsConsole.log "Executing updated test: ${it}" }
+            compile()
+            runTests(modifiedFiles)
+        }
+
+        lastScanned = new Date().time
         sleep(recompileFrequence * 1000)
-        compile()
     }
 }
 
-/**
- * Compile integration tests
- */
+def findUpdatedTests(long lastScanned) {
+    def files = []
+    def testDir = new File("integration", testSourceDir)
+    testDir.traverse {
+        if( it.isFile() && it.lastModified() > lastScanned ) { files << it }
+    }
+    files.collect { File f ->
+        f.absolutePath - testDir.absolutePath
+    }
+}
+
 def compile() {
     def testType = new JUnit4GrailsTestType("integration", "integration")
     def relativePathToSource = testType.relativeSourcePath
@@ -61,47 +71,36 @@ def compile() {
 }
 
 def runTests(tests) {
-    long startTime = new Date().time
-
-    def http = new HTTPBuilder( "http://${serverHost}:${serverPort}/spork/testRunner/",  )
-    http.get( path: 'run', query: [format: 'json', testPattern: tests] ) { resp, json ->
-        json.each {
-            outputResults(it, new Date().time - startTime)
+    if( tests ) {
+        long startTime = new Date().time
+        def http = new HTTPBuilder( "http://${serverHost}:${serverPort}/spork/testRunner/",  )
+        http.get( path: 'run', query: [format: 'json', testPattern: tests] ) { resp, json ->
+            json.each {
+                outputResults(it, new Date().time - startTime)
+            }
         }
     }
 }
 
 def outputResults(def results, def executionTime = 0) {
-    results.listener.failures.each { failure ->
-        grailsConsole.addStatus "Failure: "
-        grailsConsole.log "${failure.description.displayName}"
-        grailsConsole.log failure.trace
-        grailsConsole.log ""
-    }
-
-    grailsConsole.addStatus "Completed ${results.listener.finished.size()} integration tests, ${results.failCount} failed in ${executionTime}ms"
-    if( results.failCount > 0 ) {
-        grailsConsole.error "Tests FAILED"
-    }
-}
-
-def getFailedTests() {
-    File file = new File("${testReportsDir}/TESTS-TestSuites.xml")
-    if (!file.exists()) {
-        return []
-    }
-
-    def xmlParser = new XmlParser().parse(file)
-    def failedTests = xmlParser.testsuite.findAll { it.'@failures' =~ /.*[1-9].*/ || it.'@errors' =~ /.*[1-9].*/}
-
-    return failedTests.collect {
-        String testName = it.'@name'
-        testName = testName.replace('Tests', '')
-        def pkg = it.'@package'
-        if (pkg) {
-            testName = pkg + '.' + testName
+    if( !results?.equals(null) ) {
+        results.listener.failures.each { failure ->
+            grailsConsole.addStatus "Failure: "
+            grailsConsole.log "${failure.description.displayName}"
+            grailsConsole.log failure.trace
+            grailsConsole.log ""
         }
-        return testName
+
+        grailsConsole.addStatus "Completed ${results.listener.finished.size()} integration tests, ${results.failCount} failed in ${executionTime}ms"
+        if( results.failCount > 0 ) {
+            grailsConsole.error "Tests FAILED"
+        }
+        else {
+            grailsConsole.info "Tests PASSED"
+        }
+    }
+    else {
+        grailsConsole.addStatus "Completed 0 tests. Maybe the test pattern did not match any tests?"
     }
 }
 
