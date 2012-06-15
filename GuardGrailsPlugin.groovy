65882842ea3b1cfca6ea3033d661f5972dd7c6f6
Aaron Long <along@triu.com>
2012-06-14 23:55:56 -0400

    Prevent running the tests twice on startup

--- GuardGrailsPlugin.groovy
+++ GuardGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class GuardGrailsPlugin {
     // the plugin version
-    def version = "1.0.2"
+    def version = "1.0.3"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
